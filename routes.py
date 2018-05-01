4c29c9e4e6dcac739426e2cea90d776d656de5f4
Brent MacDonald <abrentmac@outlook.com>
2018-04-30 20:45:54 -0400

    added signup form

--- routes.py
+++ routes.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 from models import db
+from forms import SignupForm
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+app.secret_key = "development-key"  #generates secure forms to prevent cross-site request forgery
 
 @app.route("/")
 def index():
@@ -14,6 +17,11 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/signup")
+def signup():
+    form = SignupForm()
+    return render_template("signup.html", form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
